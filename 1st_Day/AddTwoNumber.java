b3511cfa0f82ae70589feca580b18ea2364853b7
Ankit Singh <ankitrajput868724@gmail.com>
2026-05-22 22:17:50 +0530

    JustFirstDay

---
+++ 1st_Day/AddTwoNumber.java
@@ -0,0 +1,11 @@
+public class AddTwoNumber {
+    
+public static void main(String[] args) {
+    int a = 5;
+    int b = 1;
+    int sum = a+b;
+    System.out.println("The sum of a and b is: " + sum);
+}
+
+
+}
