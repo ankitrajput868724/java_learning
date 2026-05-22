f9afefb908f33c4e96b5b9a027709f6e3cb8fef2
Ankit Singh <ankitrajput868724@gmail.com>
2026-05-22 23:20:03 +0530

    Full Array Print By Using For Loop

---
+++ 1st_Day/Arrayelementprint.java
@@ -0,0 +1,11 @@
+public class Arrayelementprint {
+    
+
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5};
+        int n = arr.length;
+        for(int i = 0 ; i<=n-1 ; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
