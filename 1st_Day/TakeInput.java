2e944d07377493be24866a0a1b82a7b04db7a1d5
Ankit Singh <ankitrajput868724@gmail.com>
2026-05-22 22:22:53 +0530

    asfd

---
+++ 1st_Day/TakeInput.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class TakeInput {
+
+    public static void main(String[] args) {
+        int a,b,c;
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.print("Enter the value of a :- ");
+        a = sc.nextInt();
+    }
+
+
+    
+}
