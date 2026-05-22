835908459005dffae16c77b45d3bdfc600383696
Ankit Singh <ankitrajput868724@gmail.com>
2026-05-22 22:27:22 +0530

    sadgtyghn

---
+++ 1st_Day/Method.java
@@ -0,0 +1,14 @@
+public class Method {
+
+  public static void Number(int a, int b){
+    int sum = a+b;
+    System.out.println("Sum is : - "+sum);
+  }
+
+
+
+    public static void main(String[] args){
+        Number(1,3);
+    }
+
+}
