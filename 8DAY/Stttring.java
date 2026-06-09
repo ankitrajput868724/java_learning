796d351eb1db526706a9ef4d89e1ca13ad3f3b86
Ankit Singh <ankitrajput868724@gmail.com>
2026-06-09 19:33:04 +0530

    asdf

---
+++ 8DAY/Stttring.java
@@ -0,0 +1,29 @@
+
+public class Stttring {
+    public static void main(String[] args) {
+        String s = "ass";
+    
+String t = "ass";
+        // if(s.equals(t)){
+        //     System.out.println("equal");
+        // }
+        // else{
+        //     System.out.println("Not equal");
+        // }
+        
+        StringBuilder sb = new StringBuilder("asdfghjkl");
+        System.out.println(sb);
+        
+        sb.setCharAt(0,'Z');
+        System.out.println(sb);
+        
+        sb.insert(1,'X' );
+        System.out.println(sb);
+    
+    
+    
+        
+            
+        }}
+        
+    
