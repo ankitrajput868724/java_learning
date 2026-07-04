4fe1433e044155ae542714afc7c919cfa521f281
Ankit Singh <ankitrajput868724@gmail.com>
2026-07-04 15:55:06 +0530

    STRINGS

---
+++ Z_CLASSES/4-7-2026/RemovingDuplicateCHAR.java
@@ -0,0 +1,26 @@
+public class RemovingDuplicateCHAR {
+public static void main(String[] args){
+
+String s = "Ankit Singh Shekhawat";
+
+String str = "AEIOUaeiou";
+String result = "";
+
+for(int i=0 ; i<str.length() ; i++){
+char ch = str.charAt(i);
+
+
+if(!s.contains(ch+"")){
+
+    result+=ch;
+
+
+
+}
+
+}
+System.out.println("REMANING VOWELS IN THIS SEQUENCE ARE   :) "+result);
+
+
+    }
+}
