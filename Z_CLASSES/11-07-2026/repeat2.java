3e860deb1ec6296c7f05bad875c262ed21294162
Ankit Singh <ankitrajput868724@gmail.com>
2026-07-11 10:47:00 +0530

    STRINGS

---
+++ Z_CLASSES/11-07-2026/repeat2.java
@@ -0,0 +1,34 @@
+public class repeat2 {
+        public static void main(String[] args) {
+        
+
+
+String s = "BOSSSSSS";
+char []ch  =s.toCharArray();
+
+
+for(int i=0 ; i<=ch.length-2 ;i++ ){
+
+    // if(ch[i+1] == ch.length-1)continue;
+    
+    if( ch[i] ==ch[i+1])
+        ch[i+1] = '@';
+    
+}
+System.out.println(ch);
+
+
+
+
+// for(int i=0 ; i<ch.length ; i++){
+//     if(ch[i] == ' ') continue;
+//     for(int j =i+1 ; j<ch.length ; j++){
+//         if(ch[i] ==ch[j]){
+//             ch[j] = '_';}
+    
+//     }
+    // System.out.print(ch[i]);
+}
+    }
+    
+
