27dd2e1c6880a1acca1296c06152876810a399e5
Ankit Singh <ankitrajput868724@gmail.com>
2026-07-17 22:17:09 +0530

    OOPS

---
+++ oops/dfg2.java
@@ -0,0 +1,26 @@
+public class dfg2{
+
+
+    int a ;
+
+    void m1(){
+        System.out.println(a);
+    }
+
+    public static void main(String[] args) {
+        
+dfg2 oo = new dfg2();
+oo.a=6756;
+oo.m1();
+
+dfg2 oo2 = new dfg2();
+
+oo2.a = 20;
+oo2.m1();
+
+
+oo.m1();
+oo2.m1();
+}
+}
+
